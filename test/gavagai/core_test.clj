510406fa1d50382f1ada8a854eba1e289cef39bf
Nils Grunwald <nils@grunwald.fr>
2012-08-12 01:11:48 +0200

    added some tests

--- test/gavagai/core_test.clj
+++ test/gavagai/core_test.clj
@@ -3,15 +3,22 @@
   (:require [gavagai.core :as g]))
 
 (g/register-converters
- ["java.util.Date" :exclude [:class] :add {:string str}])
+ ["java.util.Date" :exclude [:class] :add {:string str}]
+ ["java.awt.Color" :only [:green :red :blue] :add {:string str} :lazy? false]
+)
 
 (deftest basic-tests
   (testing "basic translations tests"
     (let [dt (java.util.Date.)]
-      (is (= 1 (g/register-converters
-                ["java.util.Date" :exclude [:class] :add {:string str}])))
       (g/with-translator-ns gavagai.core-test
-        (let [t (g/translate dt)]
-          (is (= (.getMinutes dt) (:minutes t)))
-          (is (nil? (:class t)) )
-          (is (= (.toString dt) (:string t))))))))
+        (let [tdt (g/translate dt)]
+          (is (instance? lazymap.core.LazyPersistentMap tdt))
+          (is (= (.getMinutes dt) (:minutes tdt)))
+          (is (nil? (:class tdt)) )
+          (is (= (.toString dt) (:string tdt)))))))
+  (testing "testing non lazy translation and only"
+    (let [c (java.awt.Color. 10 10 10)]
+      (g/with-translator-ns gavagai.core-test
+        (let [tc (g/translate c)]
+          (is (instance? clojure.lang.PersistentArrayMap tc))
+          (is (= #{:green :red :blue :string} (into #{} (keys tc)))))))))
