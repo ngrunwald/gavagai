29de7927395c4df73b73c0b6ed70ebdca53605df
Nils Grunwald <nils@grunwald.fr>
2012-11-20 12:00:04 +0100

    added test for namespace isolation

--- test/gavagai/core_test.clj
+++ test/gavagai/core_test.clj
@@ -6,10 +6,6 @@
  ["java.util.Date" :add {:string str}]
  ["java.awt.Color" :only [:green #"r.d" :blue] :add {:string str} :lazy? false])
 
-(g/register-converters {:exclude [:class]}
- ["java.awt.Button" :translate-arrays? true :lazy? false]
- ["java.awt.Button$AccessibleAWTButton" :exclude [#"loc.le"] :lazy? false])
-
 (deftest basic-tests
   (testing "basic translations tests"
     (let [dt (java.util.Date.)]
@@ -24,7 +20,13 @@
       (g/with-translator-ns gavagai.core-test
         (let [tc (g/translate c)]
           (is (instance? clojure.lang.PersistentArrayMap tc))
-          (is (= #{:green :red :blue :string} (into #{} (keys tc))))))))
+          (is (= #{:green :red :blue :string} (into #{} (keys tc)))))))))
+
+(g/register-converters {:exclude [:class]}
+ ["java.awt.Button" :translate-arrays? true :lazy? false]
+ ["java.awt.Button$AccessibleAWTButton" :exclude [#"loc.le"] :lazy? false])
+
+(deftest greedy-tests
   (testing "testing recursivity, max-depth and array translations"
     (let [b (java.awt.Button. "test")]
       (g/with-translator-ns gavagai.core-test
@@ -33,3 +35,18 @@
           (is (instance? java.awt.Button$AccessibleAWTButton
                          (get-in tb [:accessible-context :accessible-action :accessible-action])))
           (is (vector? (:action-listeners tb))))))))
+
+(in-ns 'gavagai.other-core-test)
+
+(gavagai.core/register-converters {:exclude [:class] :lazy? false}
+                       ["java.util.Date" :only [:time]])
+
+(in-ns 'gavagai.core-test)
+
+(deftest other-ns-tests
+  (testing "namespace isolation"
+    (let [dt (java.util.Date.)]
+      (gavagai.core/with-translator-ns gavagai.other-core-test
+        (let [tdt (gavagai.core/translate dt)]
+          (is (integer? (:time tdt)))
+          (is (= 1 (count tdt))))))))
