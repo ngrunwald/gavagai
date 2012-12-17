c4f45070845a3a96da7c665ef3adfe70677c3b57
Nils Grunwald <nils@grunwald.fr>
2012-12-17 19:38:27 +0100

    use new stable lazymap

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [de.kotka/lazymap "3.0.0"]])
+                 [fr.grunwald/lazymap "3.1.0"]])
