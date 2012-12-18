578aeea820edc60df75dd1b136dd3f5cc387de28
Nils Grunwald <nils@grunwald.fr>
2012-12-18 12:43:20 +0100

    updated version for release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject gavagai "0.2.1-SNAPSHOT"
-  :description "Fast Clojure library to convert recursive data structures to native Clojure"
+(defproject gavagai "0.2.1"
+  :description "Fast Clojure library to convert deep java objects structures to native Clojure"
   :url "https://github.com/ngrunwald/gavagai"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
